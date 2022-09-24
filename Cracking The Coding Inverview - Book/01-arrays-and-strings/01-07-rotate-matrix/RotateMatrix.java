6130a8907cee2e67893090f129113028ca4f7d2b
Renzo Belon <renzobelon@gmail.com>
2022-09-24 11:48:27 -0500

    Added RotateMatric.java solution

--- Cracking The Coding Inverview - Book/01-arrays-and-strings/01-07-rotate-matrix/RotateMatrix.java
+++ Cracking The Coding Inverview - Book/01-arrays-and-strings/01-07-rotate-matrix/RotateMatrix.java
@@ -1,6 +1,23 @@
 public class RotateMatrix {
 
-    
+    static void printMatrix(int[][] matrix) {
+
+        for (int[] row : matrix) {
+            System.out.println("---------------------");
+            System.out.print("| ");
+            for (int cell : row) {
+                if (cell < 10) {
+                    System.out.printf("0" + "%d ", cell);
+                    System.out.print("| ");
+                } else {
+                    System.out.printf("%d ", cell);
+                    System.out.print("| ");
+                }
+            }
+            System.out.print("\n");
+        }
+        System.out.println("---------------------");
+    }    
 
     public static void main(String[] args) {
 
@@ -9,7 +26,10 @@ public class RotateMatrix {
                             { 9,10,11,12}, 
                             {13,14,15,16}};
 
-        int origin = 0;
+        // Before rotation
+        System.out.println("Before rotation: ");
+        printMatrix(matrix);
+
         int layers = matrix.length / 2;
 
         // Iterate layers
@@ -19,21 +39,47 @@ public class RotateMatrix {
             int _x = i;
             int _y = i + (matrix.length-1-2*i); 
 
-            // int side = matrix.length - 2*origin;
+            // Create temporary array filled with first side
+            int[] tmp = new int[matrix.length-1-2*i];
+            for (int f = 0; f < matrix.length-1-2*i; f++) {
+                tmp[f] = matrix[x][y+f];
+            }
 
+            int[] tmp2 = new int[matrix.length-1-2*i];
+ 
             // Iterate sides
             for (int j = 0; j < 4; j++) {
                 // Iterate elements of each side
-                for (int k = 0; k < matrix.length-1-2*origin; k++) {
-                    // ... actions!!
-                    System.out.printf("%d to %d\n", matrix[x][y], matrix[_x][_y]);
+                for (int k = 0; k < matrix.length-1-2*i; k++) {
+                    // System.out.printf("%d to %d\n", matrix[x][y], matrix[_x][_y]);
+
+                    if (j==0) {
+                        // Store next side on temporary array
+                        tmp[k] = matrix[_x][_y];
+                        // Overwrite next side with current side
+                        matrix[_x][_y] = matrix[x][y];
+                    }
+                    
+                    if (j>0) {
+                        // Store next side on tmp array 2
+                        tmp2[k] = matrix[_x][_y];
+                        // Overwrite next side with tmp array 1
+                        matrix[_x][_y] = tmp[k];
+                        // Overwrite tmp array 1 with tmp array 2
+                        tmp[k] = tmp2[k];
+                    }
+
+                    // Updates matrix pointers
                     if (j == 0) {y++; _x++;}
                     if (j == 1) {x++; _y--;}
                     if (j == 2) {y--; _x--;}
                     if (j == 3) {x--; _y++;}
                 }
             }
-            origin++;
         }
+
+        // After rotation
+        System.out.println("After rotation: ");
+        printMatrix(matrix);
     }
 }
